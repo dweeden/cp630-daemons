61bdb184314cc2fa20a0d2b07e83619a4b98e14b
dweeden <dave@weeden.ca>
2020-12-22 13:32:05 -0800

    graceful shutdown daemon

--- relay_watcher.py
+++ relay_watcher.py
@@ -3,27 +3,31 @@ import mysql.connector
 from mysql.connector import Error
 import RPi.GPIO as GPIO
 
-DELAY = 0.5
+LOOP_SLEEP = 0.5
 PIN_LIST = [3, 4, 17, 27, 22, 10, 9, 11]
-MOST_RECENT_STATE_CHANGE_REQUEST_QUERY = "select * from relay_setting order by created_at desc limit 1"
 DATABASE_HOST = "localhost"
 DATABASE_NAME = "worker"
 DATABASE_USER = "cp630"
 DATABASE_PASSWORD = "cp630"
+MOST_RECENT_STATE_CHANGE_REQUEST_QUERY = "select * from relay_setting order by created_at desc limit 1"
 
+# initialize pins to off
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_LIST, GPIO.OUT)
 GPIO.output(PIN_LIST, GPIO.HIGH)
 
-connection = None
 cursor = None
+connection = None
 lastPattern = ""
 try:
-    connection = mysql.connector.connect(host=DATABASE_HOST, database=DATABASE_NAME, user=DATABASE_USER, password=DATABASE_PASSWORD)
-    if connection.is_connected():
-        print('Connected to worker database')
 
-        # open cursor and update transaction isolation level so we can see results from others
+    # connect to database
+    connection = mysql.connector.connect(host=DATABASE_HOST, database=DATABASE_NAME, user=DATABASE_USER, password=DATABASE_PASSWORD, auth_plugin='mysql_native_password')
+
+    if connection.is_connected():
+        print("Connected to worker database")
+
+        # open cursor and update transaction isolation level so we can see results from other processes
         cursor = connection.cursor()
         cursor.execute("set session transaction isolation level read committed")
 
@@ -31,10 +35,10 @@ try:
 
             # fetch most recent state change request
             cursor.execute(MOST_RECENT_STATE_CHANGE_REQUEST_QUERY)
-            mostRecent = cursor.fetchone()
+            mostRecentSCR = cursor.fetchone()
 
-            if mostRecent is not None:
-                pattern = mostRecent[3]
+            if mostRecentSCR is not None:
+                pattern = mostRecentSCR[3]
                 if pattern != lastPattern:
                     # determine on/off lists
                     on = []
@@ -48,25 +52,30 @@ try:
                     # set relay states, applying off list first to minimize the
                     # chance of two incompatible things being on simultaneously
                     # NOTE: relay board is active low
-                    print(f"updating relay states to -> {pattern}")
+                    print(f"Updating relay states to -> { pattern }")
                     GPIO.output(off, GPIO.HIGH)
                     GPIO.output(on, GPIO.LOW)
 
                     # save last pattern to reduce unnecessary calls to gpio
                     lastPattern = pattern
 
-            time.sleep(DELAY)
+            time.sleep(LOOP_SLEEP)
+
     else:
         print("Couldn't connect to to worker database")
 
 except Error as e:
+    print("Exception encountered")
     print(e)
 
 finally:
     # cleanup
+    print("Cleaning up")
     if cursor is not None:
         cursor.close()
     if connection is not None and connection.is_connected():
         connection.close()
     GPIO.output(PIN_LIST, GPIO.HIGH)
     GPIO.cleanup()
+
+print("Script has completed")
